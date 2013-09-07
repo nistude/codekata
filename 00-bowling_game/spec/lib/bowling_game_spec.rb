31b011229e772f2ab761a01cdf050d5bd75f06b4
Nikolay Sturm <sturm@erisiandiscord.de>
2013-09-07 16:04:14 +0200

    proper initial score

--- 00-bowling_game/spec/lib/bowling_game_spec.rb
+++ 00-bowling_game/spec/lib/bowling_game_spec.rb
@@ -1,11 +1,17 @@
 require 'bowling_game'
 
 describe BowlingGame do
+  let(:game) { BowlingGame.new }
+
   describe '#roll' do
 
   end
 
   describe '#score' do
-    
+    context 'without any rolls' do
+      it 'returns 0' do
+        expect(game.score).to eq 0
+      end
+    end
   end
 end
