358eb23696b1eefce4a4f7ec9fe69150cd4d4f60
Nikolay Sturm <sturm@erisiandiscord.de>
2013-09-07 20:55:30 +0200

    more refactorings

--- 00-bowling_game/lib/bowling_game.rb
+++ 00-bowling_game/lib/bowling_game.rb
@@ -2,27 +2,19 @@ class BowlingGame
   def initialize
     @frames = []
     @current_frame = -1
-    @rolls = 0
+    @rolls = -1
   end
 
   def roll(pins)
-    if new_frame?
-      @current_frame += 1
-      @frames[@current_frame] = []
-    end
-    @frames[@current_frame] << pins
     @rolls += 1
+    setup_new_frame if new_frame?
+    @frames[@current_frame] << pins
   end
 
   def score
     game_score = 0
     @frames.each_with_index do |frame, index|
-      @frame_score = 0
-      frame.each { |pins| @frame_score += pins }
-      if spare?
-        @frame_score += @frames[index + 1][0]
-      end
-      game_score += @frame_score
+      game_score += frame_score(frame, index)
     end
     game_score
   end
@@ -30,6 +22,20 @@ class BowlingGame
   private
   def new_frame?
     @rolls % 2 == 0
+  end
+
+  def setup_new_frame
+    @current_frame += 1
+    @frames[@current_frame] = []
+  end
+
+  def frame_score(frame, index)
+    @frame_score = 0
+    frame.each { |pins| @frame_score += pins }
+    if spare?
+      @frame_score += @frames[index + 1][0]
+    end
+    @frame_score
   end
 
   def spare?
