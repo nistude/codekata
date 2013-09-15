6d23c974871f34e4ef26fb0bd4a049fc696fd7ed
Nikolay Sturm <sturm@erisiandiscord.de>
2013-09-15 08:05:52 +0200

    pass gutter game

--- 01-bowling_game/spec/lib/bowling_game_spec.rb
+++ 01-bowling_game/spec/lib/bowling_game_spec.rb
@@ -1,11 +1,16 @@
 require 'bowling_game'
 
 describe BowlingGame do
+  let(:game) { BowlingGame.new }
+
   describe '#roll' do
 
   end
 
   describe '#score' do
-    
+    it 'returns 0 for the gutter game' do
+      20.times { game.roll(0) }
+      expect(game.score).to eq 0
+    end
   end
 end
