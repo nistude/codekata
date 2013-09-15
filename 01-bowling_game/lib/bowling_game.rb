772e1a70aa24a9e740cab2a9bd03c6a54160e3cd
Nikolay Sturm <sturm@erisiandiscord.de>
2013-09-15 11:56:23 +0200

    refactor

--- 01-bowling_game/lib/bowling_game.rb
+++ 01-bowling_game/lib/bowling_game.rb
@@ -8,47 +8,38 @@ class BowlingGame
   end
 
   def score
-    game_score = 0
+    game_score, roll = 0, 0
     frame = 1
-    i = 0
     while frame <= 10 do
-      pins = @rolls[i]
-      if pins == 10
-        frame_score = pins
-        frame_score += @rolls[i + 1]
-        frame_score += @rolls[i + 2]
-        frame += 1
-        i += 1
-      elsif pins + @rolls[i + 1] == 10
-        frame_score = pins + @rolls[i + 1]
-        frame_score += @rolls[i + 2]
-        frame += 1
-        i += 2
+      first_roll = @rolls[roll]
+      second_roll = @rolls[roll + 1] || 0
+
+      if strike?(first_roll)
+        frame_score = first_roll
+        frame_score += @rolls[roll + 1]
+        frame_score += @rolls[roll + 2]
+        roll += 1
+      elsif spare?(first_roll, second_roll)
+        frame_score = first_roll + second_roll
+        frame_score += @rolls[roll + 2]
+        roll += 2
       else
-        frame_score = pins + @rolls[i + 1]
-        frame += 1
-        i += 2
+        frame_score = first_roll + second_roll
+        roll += 2
       end
       game_score += frame_score
+
+      frame += 1
     end
     game_score
   end
 
   private
-  def frame_score(frame)
-    first_roll = @rolls[2 * frame]
-    second_roll = @rolls[2 * frame + 1]
-    frame_score = first_roll + second_roll
-
-    if spare?(first_roll, second_roll)
-      bonus = @rolls[2 * frame + 2]
-      frame_score += bonus
-    end
-
-    frame_score
+  def strike?(first_roll)
+    first_roll == 10
   end
 
-  def spare?(*frame)
-    frame[0] + frame[1] == 10
+  def spare?(first_roll, second_roll)
+    first_roll + second_roll == 10
   end
 end
